b74c2d2e072ac5ff99b5ce0f048e8a03121e7f7e
lootb0x <justinlin8031@gmail.com>
2025-05-03 12:58:52 +0000

    dsadasd

--- Main.java
+++ Main.java
@@ -3,8 +3,15 @@ public class Main{
      {
         Textbook bio2015 = new Textbook( "Biology", 49.75, 2);
         Textbook bio2019 = new Textbook( "Biology", 39.75, 3);
+        Textbook math = new Textbook("calculus", 45.25, 1);
 
-        System.out.println(bio2015.getEdition());
+        System.out.println(bio2019.getEdition());
         System.out.println(bio2019.getInfo());
+        System.out.println(bio2019.subsitutes(bio2015));
+        System.out.println(bio2015.subsitutes(bio2019));
+        System.out.println(bio2015.subsitutes(math));
+
+
+
      }
 }
